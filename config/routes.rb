3b2433b33604e58c3d05b1e92e82959aaee7e4d4
Jviveiros <jviveiros@github.com>
2017-01-15 14:15:49 -0800

    Built out routes for MU blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resource :users do
+    resource :articles
+  end
+  resource :articles do
+    resource :comments, only: [:new, :create, :edit, :delete]
+  end
+
+  resource :sessions, only: [:new, :create, :delete]
+
+  root 'articles#index'
 end
