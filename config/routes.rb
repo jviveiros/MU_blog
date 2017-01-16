18eea92a225548a3ef512ea5a3b65d3d9e93f97a
Jviveiros <jviveiros@github.com>
2017-01-16 11:09:55 -0800

    Login, Logout and User Creation all working

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
     resource :comments, only: [:new, :create, :edit, :delete]
   end
 
-  resource :sessions, only: [:new, :create, :delete]
+  resource :sessions, only: [:new, :create, :destroy]
+
+  delete '/log_out', to: 'sessions#destroy', as: :log_out
+  get '/log_in', to: 'sessions#new', as: :log_in
 
   root 'articles#index'
 end
