1ca5118b2a88e6f6a8b2cf3e68e624c96870fbd6
Jviveiros <jviveiros@github.com>
2017-01-16 09:43:48 -0800

    Got user Creation working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,12 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find_by(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
 end
