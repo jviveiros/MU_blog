ee216a9494cc212b489d62123dabfe9cd728e138
Jviveiros <jviveiros@github.com>
2017-01-17 16:11:18 -0800

    Added Articles and Comments to User Profile Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
+    @articles = @user.articles
+    @comments = @user.comments
   end
 
   def destroy
